fdf34e978c8f6015c957b260074ab75f2303711c
Yuta iwasaki <yanyong0127@gmail.com>
2021-06-26 16:06:00 +0900

    Initial Commit

---
+++ aboutEachUIPart1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aboutEachUIPart1
+//
+//  Created by IwasakIYuta on 2021/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
